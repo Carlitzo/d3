2fec8e20ef761f1b1e1439a53de43e3308f911ea
Carlitzo <adrianscholl95@hotmail.com>
2025-05-17 12:50:24 +0200

    Sorted buttons
    Added renderTopButtons function as well as filterFunctions for years,ethnicities and genders as well as CSS for buttons in general

--- public/index.js
+++ public/index.js
@@ -1,5 +1,9 @@
 import { renderSvg } from "./renderingFunctions/renderSvg/renderSvg.js";
 import { renderCities } from "./renderingFunctions/renderCities/renderCities.js";
+import { renderTopButtons } from "./renderingFunctions/renderTopButtons/renderTopButtons.js";
 
 renderSvg("upperMap");
 renderCities();
+renderTopButtons("ethnicityButtonsContainer");
+renderTopButtons("genderButtonsContainer");
+renderTopButtons("yearButtonsContainer");
