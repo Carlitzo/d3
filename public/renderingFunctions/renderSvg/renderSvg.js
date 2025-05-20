830dcc85c327fb3d39487bc241569c9e6fb8a347
esmir97 <esmiri97@hotmail.com>
2025-05-20 23:12:16 +0200

    Changed renderSvg function
    - Now selects all elements with class .mapSvg
    - Loops through both elements and creates and appends the svg&node to both
    - Creates a clone of the node so it can be appended to both svg:s

--- public/renderingFunctions/renderSvg/renderSvg.js
+++ public/renderingFunctions/renderSvg/renderSvg.js
@@ -1,14 +1,7 @@
 export function renderSvg() {
 
-    const parent = document.querySelector(".mapSvg");
-    const svg = d3.select(parent)
-        .append("svg")
-        .attr("width", "100%")
-        .attr("height", "100%");
-
-    const g = svg.append("g")
-        .attr("transform", "scale(0.9)");
-
+    const parents = document.querySelectorAll(".mapSvg");
+    
     fetch("./images/china.svg")
         .then(r => r.text())
         .then(text => {
@@ -16,12 +9,25 @@ export function renderSvg() {
             const svgDoc = parser.parseFromString(text, "image/svg+xml");
             const importedSvg = svgDoc.documentElement;
 
-            g.node().appendChild(importedSvg);
+            parents.forEach( (parent) => {
 
-            d3.select(importedSvg).selectAll("path")
-                .attr("fill", "#C7C7C7")
-                .attr("stroke", "white")
-                .attr("stroke-width", 1);
+                const svg = d3.select(parent)
+                    .append("svg")
+                    .attr("width", "100%")
+                    .attr("height", "100%");
+
+                const g = svg.append("g")
+                    
+
+                const clone = importedSvg.cloneNode(true);
+
+                g.node().appendChild(clone);
+
+                d3.select(clone).selectAll("path")
+                    .attr("fill", "#C7C7C7")
+                    .attr("stroke", "white")
+                    .attr("stroke-width", 1);
+            });
         })
         .catch(err => console.error("Failed to load SVG:", err));
 }
